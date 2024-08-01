6af06a111e17bb19555c803ccb058f15ee6c5376
Chris Martin <chris.martin@iterable.com>
2024-08-01 11:38:19 -0700

    work so far

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,10 +1,17 @@
 // Rewrite the factorial function using a `while` loop.
-pub fn factorial(n: u32) -> u32 {
+pub fn factorial(mut n: u32) -> u32 {
     // The `todo!()` macro is a placeholder that the compiler
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut sum = 1;
+
+    while n > 0 {
+        sum *= n; // sum = sum * n
+        n -= 1; // n = n - 1
+    }
+
+    return sum;
 }
 
 #[cfg(test)]
