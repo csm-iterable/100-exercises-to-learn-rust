9ab1568a9920e97a617c877f43fa5059afc21e00
Chris Martin <chris.martin@iterable.com>
2024-08-15 16:12:22 -0700

    fancy validation with optionals

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,7 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        Self::perform_validation(Some(&title), Some(&description), Some(&status));
 
         Ticket {
             title,
@@ -34,16 +20,72 @@ impl Ticket {
         }
     }
 
+    fn perform_validation(
+        title: Option<&String>,
+        description: Option<&String>,
+        status: Option<&String>,
+    ) {
+        match title {
+            None => (),
+            Some(title) => {
+                if title.len() > 50 {
+                    panic!("Title cannot be longer than 50 bytes");
+                }
+
+                if title.is_empty() {
+                    panic!("Title cannot be empty");
+                }
+            }
+        }
+
+        match description {
+            None => (),
+            Some(description) => {
+                if description.len() > 500 {
+                    panic!("Description cannot be longer than 500 bytes");
+                }
+
+                if description.is_empty() {
+                    panic!("Description cannot be empty")
+                }
+            }
+        }
+
+        match status {
+            None => (),
+            Some(status) => {
+                if status != "To-Do" && status != "In Progress" && status != "Done" {
+                    panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+                }
+            }
+        }
+    }
+
     pub fn title(&self) -> &String {
         &self.title
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Self::perform_validation(Some(&new_title), None, None);
+        self.title = new_title;
     }
 
     pub fn description(&self) -> &String {
         &self.description
     }
 
+    pub fn set_description(&mut self, new_description: String) {
+        Self::perform_validation(None, Some(&new_description), None);
+        self.description = new_description;
+    }
+
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Self::perform_validation(None, None, Some(&new_status));
+        self.status = new_status;
     }
 }
 
@@ -80,14 +122,14 @@ mod tests {
     #[should_panic(expected = "Title cannot be longer than 50 bytes")]
     fn title_cannot_be_longer_than_fifty_chars() {
         Ticket::new(valid_title(), valid_description(), "To-Do".into())
-            .set_title(overly_long_title())
+            .set_title(overly_long_title());
     }
 
     #[test]
     #[should_panic(expected = "Description cannot be longer than 500 bytes")]
     fn description_cannot_be_longer_than_500_chars() {
         Ticket::new(valid_title(), valid_description(), "To-Do".into())
-            .set_description(overly_long_description())
+            .set_description(overly_long_description());
     }
 
     #[test]
